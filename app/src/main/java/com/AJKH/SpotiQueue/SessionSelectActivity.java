161d4cf56c3d1e951bfb7e4b97740d29882452a8
Jacob Sturm <aj.sturm10@gmail.com>
2016-10-31 20:29:17 -0400

    Revert "Revert "Add new opening screen with buttons to create or join a session (non-functional)""
    This reverts commit 5d80b6a3385ebfe5a8a30a21bbccd1cf8b215827.

---
+++ app/src/main/java/com/AJKH/SpotiQueue/SessionSelectActivity.java
@@ -0,0 +1,63 @@
+package com.AJKH.SpotiQueue;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.AJKH.SpotiQueue.Firebase.SignInActivity;
+import com.AJKH.SpotiQueue.Spotify.SpotifyHttpUtil;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Created by jsturm on 10/31/2016.
+ */
+
+public class SessionSelectActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
+
+    private Button joinSessionButton;
+    private Button createSessionButton;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.session_select);
+
+        // Initialize Firebase Auth
+        FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+        }
+
+        joinSessionButton = (Button) findViewById(R.id.joinSessionButton);
+        joinSessionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SessionSelectActivity.this, MainActivity.class));
+            }
+        });
+
+        createSessionButton = (Button) findViewById(R.id.createSessionButton);
+        createSessionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SessionSelectActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d("SessionSelectActivity", "onConnectionFailed:" + connectionResult);
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+    }
+}
